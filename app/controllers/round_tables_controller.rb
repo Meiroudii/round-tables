5a1ce07726e52a6a7bc84148d40a5915a66d41e1
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2026-01-14 10:48:30 +0900

    config: add user sign

--- app/controllers/round_tables_controller.rb
+++ app/controllers/round_tables_controller.rb
@@ -1,6 +1,6 @@
 class RoundTablesController < ApplicationController
   before_action :authenticate_user!, except: [ :index, :show ]
-  before_action :set_round_table, only: %i[ show edit update destroy ]
+  before_action :set_round_table, only: %i[ show edit ]
   def index
     @round_tables = RoundTable.all
   end
