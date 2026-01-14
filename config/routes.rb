529d8f9b77641d6fd51203d63a50746dc7b0ce61
Meiroudii <119019700+Meiroudii@users.noreply.github.com>
2026-01-14 10:52:27 +0900

    style: add form placeholder

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :round_tables
-  resources :users, only: [ :show ]
+  resources :users
   root "round_tables#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
